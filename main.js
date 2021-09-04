76b5ebabd1d6fe57bbad333149a1c0b3c1aeed08
mochuvfx <87718949+mochuvfx@users.noreply.github.com>
2021-09-04 13:49:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+noseX = 0
+noseY = 0
+
+function preload() 
+{
+clown_nose = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup() 
+{
+canvas = createCanvas(300, 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300, 300);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    Image(video, 0, 0, 300, 300);
+    Image(clown_nose, noseX, noseY, 80, 35);
+} 
+
